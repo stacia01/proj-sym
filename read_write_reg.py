097dbd3d3cd9855db58ad887c355fa094d96e163
stac <staciarimta@gmail.com>
2022-11-05 18:51:58 +0700

    change to float

--- read_write_reg.py
+++ read_write_reg.py
@@ -2,11 +2,11 @@ import minimalmodbus
 
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
 
-pressure_in = instrument.read_register(registeraddress=1, functioncode=3)
-pressure_out = instrument.read_register(registeraddress=2, functioncode=3)
-temperature_out = instrument.read_register(registeraddress=3, functioncode=3)
-totaliser = instrument.read_register(registeraddress=4, functioncode=3)
-status = instrument.read_register(registeraddress=5, functioncode=3)
+pressure_in = instrument.read_register(registeraddress=1, functioncode=3, number_of_decimals=1)
+pressure_out = instrument.read_register(registeraddress=2, functioncode=3, number_of_decimals=1)
+temperature_out = instrument.read_register(registeraddress=3, functioncode=3, number_of_decimals=1)
+totaliser = instrument.read_register(registeraddress=4, functioncode=3, number_of_decimals=1)
+status = instrument.read_register(registeraddress=5, functioncode=3, number_of_decimals=1)
 
 print(pressure_in)
 print(pressure_out)
